acc1a10b1f2f56c249474a1d3bb1fd8d7cd53754
SukhmanSra <ssra5@uwo.ca>
2025-01-19 00:15:39 -0500

    cors again

--- app/server/app.py
+++ app/server/app.py
@@ -26,6 +26,7 @@ db = get_db()
 
 app.add_middleware(
     CORSMiddleware,
+    allow_origins=["http://localhost:3000"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
