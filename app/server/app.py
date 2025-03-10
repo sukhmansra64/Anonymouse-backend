c9ed8139314dd94f6f60b729e799a7dba24fd341
SukhmanSra <ssra5@uwo.ca>
2025-03-09 23:10:04 -0400

    Testing

--- app/server/app.py
+++ app/server/app.py
@@ -66,6 +66,8 @@ async def connect(sid, environ):
             room=user_id
         )
 
+        print("Rooms:", socket_manager.rooms(sid))
+
         print(f"User {user_id} connected via socket: {sid}")
     except (JWTError, ConnectionRefusedError) as e:
         print(f"Connection refused: {e}")
