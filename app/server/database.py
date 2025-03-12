6d13817d850b3b3eb8020b7fc65c570333f8d0e0
SukhmanSra <ssra5@uwo.ca>
2025-03-12 00:27:30 -0400

    Changing db back

--- app/server/database.py
+++ app/server/database.py
@@ -11,7 +11,7 @@ client = AsyncIOMotorClient(URI) if URI else None
 def get_db():
     if not client:
         raise ValueError("MongoDB client is not initialized. Check your DB_URI environment variable.")
-    return client["Sukhman-RND"]
+    return client["Anonymouse"]
 
 
 
